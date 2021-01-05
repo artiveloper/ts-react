b5c72c606915ecdbba260f2a3dd5fb3aa30c7423
Artiveloper <artiveloper@gmail.com>
2021-01-05 15:26:06 +0900

    feat: 구구단 게임 완성

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "development", // production
+  devtool: "eval", // hidden-source-map
+  resolve: {
+    extensions: [".jsx", ".js", ".tsx", ".ts"],
+  },
+
+  entry: {
+    app: "./src/index",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader",
+      },
+    ],
+  },
+  plugins: [],
+  output: {
+    path: path.join(__dirname, "/public/dist"),
+    filename: "app.js",
+  },
+  devServer: {
+    contentBase: __dirname + "/public",
+    inline: true,
+    hot: true,
+    host: "localhost",
+    port: 3000
+  }
+};
